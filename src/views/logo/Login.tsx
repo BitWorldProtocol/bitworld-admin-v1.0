78937ae12f2d629f13d0c11edf1aa4a7f200b785
BitWorldProtocol <cyptojune@gmail.com>
2024-05-07 21:48:23 +0800

    gradient button

--- src/views/logo/Login.tsx
+++ src/views/logo/Login.tsx
@@ -1,9 +1,16 @@
 import styles from  "./index.module.less";
-import { Button, Form, Input, message } from 'antd';
+import { Button, Form, Input, message, ConfigProvider } from 'antd';
 import api from "@/api";
+import { TinyColor } from '@ctrl/tinycolor';
 import { Login } from "@/types/api";
 import storage from "@/utils/storage";
 import { useState } from "react";
+
+const colors3 = ['#40e495', '#30dd8a', '#2bb673'];
+const getHoverColors = (colors: string[]) =>
+  colors.map((color) => new TinyColor(color).lighten(10).toString());
+const getActiveColors = (colors: string[]) =>
+  colors.map((color) => new TinyColor(color).darken(10).toString());
 
 export default function LoginFC() {
   const [loading, setLoading] = useState(false)
@@ -44,9 +51,22 @@ export default function LoginFC() {
           </Form.Item>
 
           <Form.Item>
-            <Button type="primary" block htmlType="submit" loading={loading}>
-              Login
-            </Button>
+            <ConfigProvider
+              theme={{
+                components: {
+                  Button: {
+                    colorPrimary: `linear-gradient(116deg,  ${colors3.join(', ')})`,
+                    colorPrimaryHover: `linear-gradient(116deg, ${getHoverColors(colors3).join(', ')})`,
+                    colorPrimaryActive: `linear-gradient(116deg, ${getActiveColors(colors3).join(', ')})`,
+                    lineWidth: 0,
+                  },
+                },
+              }}
+              >
+              <Button type="primary" block htmlType="submit" loading={loading}>
+                Login
+              </Button>
+            </ConfigProvider>
           </Form.Item>
         </Form>
       </div>
